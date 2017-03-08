6aa8c5b4e0ed6420aaaf5b85bc953a8c35332ec2
Hugo Peres <hugo-pdf@hotmail.com>
2017-03-08 13:49:49 -0300

    Add manifest offline and app.manifest

---
+++ public/service-worker.js
@@ -0,0 +1,15 @@
+let version = 1;
+
+var files = ['./', '']
+
+];
+
+
+self.addEventListener('install', function() {})
+
+self.addEventListener('', function() {})
+
+self.addEventListener('install', function() {})
+
+
+function 
