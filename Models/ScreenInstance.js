780341f89dfd0730012e9117013c230ee8ff850d
Hugo Peres <hugo-pdf@hotmail.com>
2017-12-15 08:26:05 -0200

    update files

---
+++ Models/ScreenInstance.js
@@ -0,0 +1,40 @@
+const Instance = function (dataBuffer) {
+
+    if (!dataBuffer || !dataBuffer.length){
+        return console.error("There is no Buffer from PLC!");
+    } 
+
+    let instNameSize = dataBuffer.readUInt8(1,2);
+    this.instName = dataBuffer.slice(2, (2 + instNameSize)).toString('utf-8') || null;
+    this.lineTakt = dataBuffer.readInt32BE(18, 22) || 0; // Takt da linha
+    this.lineStopTime = dataBuffer.readInt32BE(22, 26) || null; // Stop time da linha
+    this.produced = dataBuffer.readInt16BE(26, 28) || null; // Produzidos na linha
+    this.objective = dataBuffer.readInt16BE(28, 30) || null; // Objetivo do Dia
+    this.lineStopPlan = dataBuffer.readInt32BE(30, 34) || null; // Tempo de parada
+    this.logTimer = dataBuffer.readInt32BE(34, 38) || null; // Tempo da logistica
+    this.logStopTime = dataBuffer.readInt32BE(38, 42) || null; // Stop time da logistica
+    this.logStopPlan = (dataBuffer[42] & 0x01) > 0; // Verifica se há parada planejada na logistica, necessita de mascara para separar o Bit    
+    this.andon = (( dataBuffer[42] & 0x02) >> 1) > 0 ; // dataBuffer.slice(42,44)|| null; //Verifica se há andon chamado, necessita de mascara para separar o Bit
+    this.andonMsg = dataBuffer.slice(44, 62).toString('utf-8') || null; // Mensagem de Andon
+    this.cfgTakt = dataBuffer.readInt32BE(62, 66); // Takt configurado na linha
+    this.cfgWagonNumber = dataBuffer.readInt16BE(66, 68) || null; // Numero de vagoes
+    this.cfgWagonAmount = dataBuffer.readInt16BE(68, 70) || null; // Numero de Popids por vagao    
+    
+    /* Implementando funcao para calcular a area dos vagoes */
+    let wagonNameSizes = [dataBuffer.readUInt8(73, 74), dataBuffer.readUInt8(119, 120)]
+    this.wagon = [{
+        "enabled": dataBuffer[70] & 0x01 > 0,
+        "name": dataBuffer.slice(74, (74 + wagonNameSizes[0])).toString('utf-8'),
+        "quantity": dataBuffer.readInt16BE(106, 108),
+        "timer": dataBuffer.readInt32BE(108, 112),
+        "avaliability": dataBuffer.readInt32BE(112, 116)
+    }, {
+        "enabled": dataBuffer[116] & 0x01 > 0,
+        "name": dataBuffer.slice(120, (120 + wagonNameSizes[1])).toString('utf-8'),
+        "quantity": dataBuffer.readInt16BE(152, 154),
+        "timer": dataBuffer.readInt32BE(154, 158),
+        "avaliability": dataBuffer.readInt32BE(158, 162)
+    }] || null;
+};
+
+module.exports = Instance;
---
+++ Models/TaktInstance.js
@@ -0,0 +1,35 @@
+var TaktInstance = function(dataBuffer) {
+    if (!dataBuffer || dataBuffer.length === 0) return console.error("Buffer is Empty!");
+
+    this.isActive = dataBuffer[0] & 0x01 > 0;
+    this.isPPlan = dataBuffer[0] & 0x02 > 0;
+    this.pplanTime = dataBuffer.readInt32BE(2, 6);
+    this.parameters = getTaktParameters(dataBuffer);
+    this.cycleEnd = dataBuffer[24] & 0x01;
+    this.remainingTime = dataBuffer.readInt32BE(26, 30);
+    this.production = dataBuffer.readInt16BE(30, 32);
+    this.stopTime = dataBuffer.readInt32BE(32, 36);
+    this.overBalance = dataBuffer.readInt16BE(36, 38);
+    this.accumulatedBalance = dataBuffer.readInt16BE(38, 40);
+    this.buzzer = dataBuffer[40] & 0x00;
+    this.prodComplete = dataBuffer[40] & 0x00;
+
+}
+
+function getTaktParameters(dataBuffer) {
+    var parameters = [];
+    let shifts = 3; //Numero de turnos de trabalho
+    let offset = 6;
+    let size = 6;
+    for (let i = 0; i < shifts; i++) {
+        parameters.push({
+            standTime: dataBuffer.readInt32BE(offset + 0, offset + 4),
+            prodObjective: dataBuffer.readInt16BE(offset + 4, offset + 6)
+        });
+        offset += size;
+    }
+    return parameters;
+}
+
+
+module.exports = TaktInstance;
